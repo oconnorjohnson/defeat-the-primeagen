a99282d7186f99bfb272b4cdef089a85641d98bb
Daniel Johnson <dojohnson@ucdavis.edu>
2024-04-28 20:29:34 -0700

    okee

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         src="/background.png"
         alt="background"
         className="absolute inset-0 z-100 h-screen w-screen"
-        layout="fill"
+        fill={true}
       />
       <div className="min-h-screen flex flex-col text-white text-center justify-center items-center">
         hello from the game!
