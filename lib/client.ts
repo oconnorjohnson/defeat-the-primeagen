061fc009101a4b425ac8b648030d9d1d01d71099
guitarkeegan <keegananglim@gmail.com>
2024-05-11 16:56:10 -0700

    query users on pause

--- lib/client.ts
+++ lib/client.ts
@@ -5,9 +5,12 @@ let client: Client;
 
 if (process.env.NODE_ENV === 'development') {
   // TODO: TS hell
+  // @ts-ignore
   if (!global.edgedbClient) {
+  // @ts-ignore
     global.edgedbClient = createClient();
   }
+  // @ts-ignore
   client = global.edgedbClient;
 } else {
   client = createClient();
