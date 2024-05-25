707a068a97e7210c8bd4de9a93171b71bdc5b6ca
guitarkeegan <keegananglim@gmail.com>
2024-05-24 21:04:11 -0700

    add test for user update

--- lib/achievement-actions.ts
+++ lib/achievement-actions.ts
@@ -1,7 +1,8 @@
 import {cookies} from "next/headers";
-import {client} from "@/lib/client"
+import {client} from "./client"
+import { evaluateStats } from "./utils";
 
-interface Stat {
+export interface Stat {
     score: number;
     enemy_collisions: number;
     friendly_collisions: number;
@@ -10,7 +11,7 @@ interface Stat {
     total_game_time: number;
 }
 
-interface Achievement {
+export interface Achievement {
     deez_nuts: boolean;
     skill_issues: boolean;
     brazil_mentioned: boolean;
@@ -40,85 +41,24 @@ interface Achievement {
     in_shambles: boolean;
 }
 
-async function updateAchievements(stats: Stat) {
+export async function updateAchievements(stats: Stat) {
 
-    const achievementsUpdates: string[] = [];
+    const achievementsUpdates: string[] | undefined = evaluateStats(stats);
 
+    if (!achievementsUpdates) return;
+    if (achievementsUpdates.length == 0) return;
 
-    if (stats.score === 4 && stats.enemy_collisions === 1) {
-        achievementsUpdates.push("deez_nuts := true");
-    }
-    if (stats.score === 69) {
-        achievementsUpdates.push("sixty_nine := true");
-    }
-    if (stats.score === 420) {
-        achievementsUpdates.push("four_twenty := true");
-    }
-    if (stats.friendly_misses > 10) {
-        achievementsUpdates.push("skill_issues := true");
-    }
-    if (stats.enemies_shot_down > 100) {
-        achievementsUpdates.push("hackerman := true");
-    }
-    if (stats.total_game_time > 3600) {
-        achievementsUpdates.push("netflix_btw := true");
-    }
-    if (stats.enemy_collisions > 5 && stats.friendly_collisions > 5) {
-        achievementsUpdates.push("in_shambles := true");
-    }
-    if (stats.score === 0) {
-        achievementsUpdates.push("l_take := true");
-    }
-    if (stats.score === 1) {
-        achievementsUpdates.push("got_the_w := true");
-    }
-    if (stats.enemy_collisions === 2) {
-        achievementsUpdates.push("furries := true");
-    }
-    if (stats.enemies_shot_down === 0) {
-        achievementsUpdates.push("react_andy := true");
-    }
-    if (stats.friendly_collisions === 1) {
-        achievementsUpdates.push("falcore_mentioned := true");
-    }
-    if (stats.friendly_misses === 3) {
-        achievementsUpdates.push("chad_stack := true");
-    }
-    if (stats.total_game_time < 60) {
-        achievementsUpdates.push("arch := true");
-    }
-    if (stats.enemies_shot_down === 50) {
-        achievementsUpdates.push("big_influencer_money := true");
-    }
-    if (stats.enemy_collisions === 7) {
-        achievementsUpdates.push("ocamel_mentioned := true");
-    }
-    if (stats.friendly_misses === 4) {
-        achievementsUpdates.push("functional_programming_plus_tye_dye := true");
-    }
-    if (stats.enemies_shot_down === 42) {
-        achievementsUpdates.push("zig_mentioned := true");
-    }
-    if (stats.total_game_time === 123) {
-        achievementsUpdates.push("real_talk := true");
-    }
-    if (stats.friendly_collisions === 0) {
-        achievementsUpdates.push("devon := true");
-    }
-    if (stats.score === 13) {
-        achievementsUpdates.push("tom_s_a_genius := true");
-    }
-    if (stats.enemies_shot_down === 10) {
-        achievementsUpdates.push("brazil_mentioned := true");
-    }
-    if (stats.total_game_time === 1000) {
-        achievementsUpdates.push("back_door_wang := true");
-    }
+    const updateQuery = updateAchievementsQuery(achievementsUpdates);
+    const cookieStore = cookies();
+    const authToken = cookieStore.get("edgedb-auth-token")?.value
+    if (!authToken) return undefined;
 
-    if (achievementsUpdates.length > 0) {
+    client.withGlobals({"ext::auth::client_token": authToken}).querySingle(updateQuery)
 
-    // Constructing the database query
-    const updateQuery = `
+}
+
+export function updateAchievementsQuery(achievementsUpdates: string[]): string {
+return `
         UPDATE User
         FILTER .id = global current_user.id
         SET {
@@ -132,19 +72,6 @@ async function updateAchievements(stats: Stat) {
         };
     `;
 
-    const cookieStore = cookies();
-    const authToken = cookieStore.get("edgedb-auth-token")?.value
-    if (!authToken) return undefined;
-
-    client.withGlobals({"ext::auth::client_token": authToken}).querySingle(updateQuery)
-
-    }
-}
-
-// Mock function to represent executing the query in your database
-async function executeQuery(query: string) {
-    // Implementation of your database query execution
-    console.log("Executing query:", query);
 }
 
 async function updateDbAchievements(new_score: number, laser: number, enemy_collisions: number, friendly: number) {
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import crypto from "node:crypto";
+import {Achievement, Stat, updateAchievements} from "@/lib/achievement-actions"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -29,3 +30,81 @@ export const generatePKCE = (): PKCE => {
     console.log("verifier length after ", verifier.length);
     return { verifier, challenge };
  };
+
+
+export function evaluateStats(stats: Stat): string[] | undefined {
+  const achievementsUpdates: string[] = [];
+    if (stats.score === 4 && stats.enemy_collisions === 1) {
+        achievementsUpdates.push("deez_nuts := true");
+    }
+    if (stats.score === 69) {
+        achievementsUpdates.push("sixty_nine := true");
+    }
+    if (stats.score === 420) {
+        achievementsUpdates.push("four_twenty := true");
+    }
+    if (stats.friendly_misses > 10) {
+        achievementsUpdates.push("skill_issues := true");
+    }
+    if (stats.enemies_shot_down > 100) {
+        achievementsUpdates.push("hackerman := true");
+    }
+    if (stats.total_game_time > 3600) {
+        achievementsUpdates.push("netflix_btw := true");
+    }
+    if (stats.enemy_collisions > 5 && stats.friendly_collisions > 5) {
+        achievementsUpdates.push("in_shambles := true");
+    }
+    if (stats.score === 0) {
+        achievementsUpdates.push("l_take := true");
+    }
+    if (stats.score === 1) {
+        achievementsUpdates.push("got_the_w := true");
+    }
+    if (stats.enemy_collisions === 2) {
+        achievementsUpdates.push("furries := true");
+    }
+    if (stats.enemies_shot_down === 0) {
+        achievementsUpdates.push("react_andy := true");
+    }
+    if (stats.friendly_collisions === 1) {
+        achievementsUpdates.push("falcore_mentioned := true");
+    }
+    if (stats.friendly_misses === 3) {
+        achievementsUpdates.push("chad_stack := true");
+    }
+    if (stats.total_game_time < 60) {
+        achievementsUpdates.push("arch := true");
+    }
+    if (stats.enemies_shot_down === 50) {
+        achievementsUpdates.push("big_influencer_money := true");
+    }
+    if (stats.enemy_collisions === 7) {
+        achievementsUpdates.push("ocamel_mentioned := true");
+    }
+    if (stats.friendly_misses === 4) {
+        achievementsUpdates.push("functional_programming_plus_tye_dye := true");
+    }
+    if (stats.enemies_shot_down === 42) {
+        achievementsUpdates.push("zig_mentioned := true");
+    }
+    if (stats.total_game_time === 123) {
+        achievementsUpdates.push("real_talk := true");
+    }
+    if (stats.friendly_collisions === 0) {
+        achievementsUpdates.push("devon := true");
+    }
+    if (stats.score === 13) {
+        achievementsUpdates.push("tom_s_a_genius := true");
+    }
+    if (stats.enemies_shot_down === 10) {
+        achievementsUpdates.push("brazil_mentioned := true");
+    }
+    if (stats.total_game_time === 1000) {
+        achievementsUpdates.push("back_door_wang := true");
+    }
+
+    if (achievementsUpdates.length === 0) return
+
+    return achievementsUpdates;
+}
