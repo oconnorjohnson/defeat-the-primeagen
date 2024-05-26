96ec9a3b855462dd2d2d616bcf898df3f78de44c
Daniel Johnson <dojohnson@ucdavis.edu>
2024-05-25 21:14:34 -0700

    asdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import crypto from "crypto-browserify";
-
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        ...config.resolve.fallback,
-        crypto: crypto,
-      },
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
